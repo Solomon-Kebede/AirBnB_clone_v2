cbf0efffea958f0ff54ad0991ece4c4b77ee9cf8
root <solomonaau188@gmail.com>
2022-09-29 14:10:22 -0700

    Update-2: Task 0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -28,5 +28,5 @@ mkdir -p "/data/web_static/releases/test/"
 echo "Some fake html file" > "/data/web_static/releases/test/index.html"
 ln -sf "/data/web_static/releases/test/" "/data/web_static/current"
 chown -R "ubuntu:ubuntu" "/data/"
-sudo sed -i 's/server_name _;/serverx_name _;\nlocation /hbnb_static {\n\talias /data/web_static/current/;\n\t}/g' /etc/nginx/sites-available/default
+sudo sed -i '26i\\n\location /hbnb_static {\n\talias /data/web_static/current/;\n\t}/g' /etc/nginx/sites-available/default
 sudo service nginx restart
